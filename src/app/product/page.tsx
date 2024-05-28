74924d6c3f9259da71f79b7ab9aed45cd3fd9a0c
hey-mario <lovamanitramario@gmail.com>
2024-05-28 15:26:52 +0300

    set up radix

---
+++ src/app/product/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@radix-ui/themes'
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div>
+      Product
+      <Button>New Product</Button>
+    </div>
+  )
+}
+
+export default Product
