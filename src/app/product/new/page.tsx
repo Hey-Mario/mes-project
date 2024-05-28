0c4357d913783ead71857385b0c228b4c098004f
hey-mario <lovamanitramario@gmail.com>
2024-05-28 16:17:20 +0300

    auto form with product

--- src/app/product/new/page.tsx
+++ src/app/product/new/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
+import AutoForm from '@/components/ui/auto-form'
 import React from 'react'
+import { z } from 'zod';
+
+const createProductSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().min(1).optional()
+});
 
 const NewProductPage = () => {
+
   return (
     <div>
-      NewProductPage
+      <AutoForm
+        formSchema={createProductSchema}
+      ></AutoForm>
     </div>
   )
 }
