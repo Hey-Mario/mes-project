355decdbddebc3e498e833ff5cdd98c82604d22d
hey-mario <lovamanitramario@gmail.com>
2024-06-18 15:37:12 +0300

    updated styling

--- src/app/machine/_components/MachineCard.tsx
+++ src/app/machine/_components/MachineCard.tsx
@@ -1,12 +1,13 @@
 import { IMachine } from '@/common/interfaces/IMachine';
 import React from 'react';
 import { Badge } from 'antd';
+import './style.css';
 
 const MachineCard = ({ machine }: { machine: IMachine }) => {
   return (
     <div className='py-5 px-10 border-black border-2'>
       <span>{machine.getName()}</span> <br />
-      <Badge status={machine.isOn ? 'success' : 'error'} className='size-4' />
+      <Badge status={machine.isOn ? 'success' : 'error'} className='machine-status-badge' />
     </div>
   )
 }
